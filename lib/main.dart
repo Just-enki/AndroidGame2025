1206902cb09fd71eed5440cbaff33ff030db7dbc
K0ntr4 <lpaulekat@yahoo.de>
2025-07-10 14:03:28 +0200

    added example button list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ButtonList extends StatelessWidget {
+  const ButtonList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final List<String> items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        var item = items[index];
+        return Padding(padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(item),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Game Selection';
+    return MaterialApp(
+      title: appTitle,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const Center(
+          child: ButtonList(
+            key: Key('buttonList'),
+          ),
         ),
-      ),
+      )
     );
   }
 }
